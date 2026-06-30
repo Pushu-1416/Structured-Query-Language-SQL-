7927c8e050c85173388a83646107e5db7c5b77fa
Pushu-1416 <pushpanjali1614@gmail.com>
2026-06-30 21:26:07 +0530

    Add files via upload

---
+++ datatypes in sql.sql
@@ -0,0 +1,12 @@
+use student;
+
+#create table student(studentid int,studentname varchar(100),age int,gender char(1),email varchar(100),joindate date)
+
+#create table studen(empid int,empname text,salary decimal(2),department text,hiredate date,isactive boolean)
+
+#create table product(productid int,productname text,price decimal,quantity int,manufacturedate date,expirydate date)
+
+#create table hospital(patientis int,patientname varchar(20),ade int,bloodgroup char(2),admissiondate date,billamount decimal)
+
+
+
